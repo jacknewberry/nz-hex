13fbb3683bf062df16c173f34e4d61ffa1675b5c
Jack Newberry <jack.newberry@gmail.com>
2022-02-03 14:16:23 +0000

    Add typescript, jest, and react testing
    - https://www.gatsbyjs.com/plugins/gatsby-plugin-typescript/
    - https://www.gatsbyjs.com/docs/how-to/testing/unit-testing/
    - https://www.gatsbyjs.com/docs/how-to/testing/testing-react-components/

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,55 @@
 import * as React from "react"
+import {CSSProperties} from "react";
 
 // styles
-const pageStyles = {
+const pageStyles: CSSProperties = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
-const headingStyles = {
+const headingStyles: CSSProperties = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
 }
-const headingAccentStyles = {
+const headingAccentStyles: CSSProperties = {
   color: "#663399",
 }
-const paragraphStyles = {
+const paragraphStyles: CSSProperties = {
   marginBottom: 48,
 }
-const codeStyles = {
+const codeStyles: CSSProperties = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
 }
-const listStyles = {
+const listStyles: CSSProperties = {
   marginBottom: 96,
   paddingLeft: 0,
 }
-const listItemStyles = {
+const listItemStyles: CSSProperties = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
 }
 
-const linkStyle = {
+const linkStyle: CSSProperties = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: 16,
   verticalAlign: "5%",
 }
 
-const docLinkStyle = {
+const docLinkStyle: CSSProperties = {
   ...linkStyle,
   listStyleType: "none",
   marginBottom: 24,
 }
 
-const descriptionStyle = {
+const descriptionStyle: CSSProperties = {
   color: "#232129",
   fontSize: 14,
   marginTop: 10,
@@ -62,7 +63,7 @@ const docLink = {
   color: "#8954A8",
 }
 
-const badgeStyle = {
+const badgeStyle: CSSProperties = {
   color: "#fff",
   backgroundColor: "#088413",
   border: "1px solid #088413",
@@ -139,7 +140,7 @@ const IndexPage = () => {
         </span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
+        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
         update in real-time.{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
